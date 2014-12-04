72e82cd0a2f9b19b5973f8ad1ffca3df7dbe31c0
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-12-04 08:17:04 +0100

    Updating the test runner.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -5,8 +5,9 @@ set -u
 
 script_directory=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root_directory="$script_directory/.."
+tests=ISArgumentParserTests
 
 pushd "$root_directory/Tests"
 pod update
-xcodebuild -workspace ISArgumentParserTests.xcworkspace -scheme ISArgumentParserTests clean test
+xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" clean test
 popd
