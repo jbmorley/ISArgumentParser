34c4982404138cf6f8ce1060b9f43905e4139109
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 21:37:38 +0000

    Cleaning up the build script.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -7,8 +7,9 @@ script_directory=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root_directory="$script_directory/.."
 tests=ISArgumentParserTests
 
-pushd "$root_directory/Tests"
-pod update
+pushd "$root_directory/Tests" > /dev/null
+echo "Updating CocoaPods..."
+pod update --silent
 xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" clean build | xcpretty -c
 xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" test
-popd
+popd > /dev/null
